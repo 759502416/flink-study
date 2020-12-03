97128a0aa405d71f1690a839698ab58e67609da5
759502416 <759502416@qq.com>
2020-12-03 19:26:16 +0800

    table api摸索

--- flink-website-demo/src/main/java/com/wakedata/whx/flinksql/FirstTest.java
+++ flink-website-demo/src/main/java/com/wakedata/whx/flinksql/FirstTest.java
@@ -1,18 +1,22 @@
 package com.wakedata.whx.flinksql;
 
 import com.wakedata.whx.EnvironmentUtil;
-import org.apache.flink.api.common.typeinfo.TypeHint;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
-
 import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.api.java.typeutils.TupleTypeInfo;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.table.api.Over;
+import org.apache.flink.table.api.OverWindowedTable;
 import org.apache.flink.table.api.Table;
-import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.api.Tumble;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.table.types.DataType;
+import org.apache.flink.types.Row;
 import org.junit.Test;
+
+import java.time.ZoneId;
+
+import static org.apache.flink.table.api.Expressions.$;
+import static org.apache.flink.table.api.Expressions.lit;
 
 /**
  * @author :wanghuxiong
@@ -27,14 +31,43 @@ public class FirstTest {
     public void testTable() {
         // 获得 tableEnvironment
         StreamTableEnvironment tableEnvironment = EnvironmentUtil.getTableExecutionEnvironment();
-
-       // DataType row = DataTypes.ROW(DataTypes.FIELD("name", DataTypes.STRING()), DataTypes.FIELD("onTime", DataTypes.DATE()), DataTypes.FIELD("number", DataTypes.BIGINT()));
-        //Table table = tableEnvironment.fromValues(TupleTypeInfo.of(new TypeHint<Tuple3<String, String, Long>>() {}), EnvironmentUtil.tuple3StringStringLongElements);
         DataStreamSource<Tuple3<String, String, Long>> tuple3DataStreamSource = EnvironmentUtil.getStreamExecutionEnvironment().fromCollection(EnvironmentUtil.tuple3StringStringLongElements);
         Table table = tableEnvironment.fromDataStream(tuple3DataStreamSource);
+        tableEnvironment.createTemporaryView("templeTable", table);
 
-        TableSchema schema = table.getSchema();
-        table.printSchema();
+        Table table1 = tableEnvironment.sqlQuery("select * from templeTable");
 
+        DataStream<Row> rowDataStream = tableEnvironment.toAppendStream(table1, Row.class);
+        rowDataStream.addSink(new SinkFunction<Row>() {
+            @Override
+            public void invoke(Row value, Context context) throws Exception {
+                System.err.println(value);
+            }
+        });
+        rowDataStream.addSink(new SinkFunction<Row>() {
+            @Override
+            public void invoke(Row value, Context context) throws Exception {
+                System.out.println(value);
+            }
+        });
+        try {
+            EnvironmentUtil.getStreamExecutionEnvironment().execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testProcesstime() {
+        StreamTableEnvironment tableEnvironment = EnvironmentUtil.getTableExecutionEnvironment();
+        tableEnvironment.getConfig().setLocalTimeZone(ZoneId.of("Asia/Shanghai"));
+        DataStreamSource<Tuple3<String, String, Long>> tuple3DataStreamSource = EnvironmentUtil.getStreamExecutionEnvironment().fromCollection(EnvironmentUtil.tuple3StringStringLongElements);
+        Table table = tableEnvironment.fromDataStream(tuple3DataStreamSource, $("f0").as("name"), $("f1").as("birthday")
+                , $("f2").as("numb"),$("proc_time").proctime());
+
+       // table.window(Tumble.over(lit(10).minutes()).on($("proc_time")).as("userWindow"))
+
+       // OverWindowedTable window = table.window(Over.partitionBy($("c")).orderBy($("rowTime")).preceding(lit(10).seconds()).as("ow"));
+        // table.execute().print();
     }
 }
---
+++ flink-website-demo/src/main/java/com/wakedata/whx/flinksql/ProcessTimeTest.java
@@ -0,0 +1,11 @@
+package com.wakedata.whx.flinksql;
+
+/**
+ * @author :wanghuxiong
+ * @title: ProcessTimeTest
+ * @projectName flink-study
+ * @description: TODO
+ * @date 2020/12/2 11:25 下午
+ */
+public class ProcessTimeTest {
+}
