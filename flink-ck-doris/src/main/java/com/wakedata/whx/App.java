e617fd08fd84b92b379ad00fbff9ccf6c7e0790e
王胡雄 <759502416@qq.com>
2021-06-15 20:57:43 +0800

    add click_house + doris moudle

---
+++ flink-ck-doris/src/main/java/com/wakedata/whx/App.java
@@ -0,0 +1,13 @@
+package com.wakedata.whx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
