000cd589ae1a819f21249bae5be1142632463962
wanghx <Like@0828>
2021-09-15 15:47:48 +0800

    最长公共前缀

--- leetcode/src/main/java/leetcode/editor/cn/[13]罗马数字转整数.java
+++ leetcode/src/main/java/leetcode/editor/cn/[13]罗马数字转整数.java
@@ -1,6 +1,6 @@
 package leetcode.editor.cn;
 
-public class question13 {
+class question13 {
 //罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。 
 //
 // 
--- leetcode/src/main/java/leetcode/editor/cn/[7]整数反转.java
+++ leetcode/src/main/java/leetcode/editor/cn/[7]整数反转.java
@@ -1,6 +1,6 @@
 package leetcode.editor.cn;
 
-public class question7 {
+class question7 {
 //给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。 
 //
 // 如果反转后整数超过 32 位的有符号整数的范围 [−2³¹, 231 − 1] ，就返回 0。 
